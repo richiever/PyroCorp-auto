1f487412286ac288b7217859b782acfa8dc15e47
Richie1 <33795818+richiever@users.noreply.github.com>
2023-04-04 20:04:42 +1000

    Added extra responses

--- Pyro-Bot/src/Faq/faqContent.js
+++ Pyro-Bot/src/Faq/faqContent.js
@@ -1,14 +1,22 @@
 export const faqContent = [
   {
+    question: "Discord invite link join Discord link?",
+    answer: "To join our discord go to: "
+  },
+  {
+    question: "How to use mod menu?",
+    answer: "Some common issues may be related to antivirus or firewall software. Try disabling them and see if that helps. If not, try running your mod as an administrator. If those options do not work,please contact us at"
+  },
+  {
     question: "How do I setup Stand?",
     answer: "To setup Stand, go to https://stand.gg/download and download the latest version of Stand. Then, run the installer and follow the instructions."
   },
   {
-    question: "How do I setup North?",
+    question: "How do I setup North setup help North setup?",
     answer: "To setup North, go to https://north.lol/ and make an account. Then, redeem your key and download the latest version to use North."
   },
   {
-    question: "Stand mod menu Stand not working stand mod",
+    question: "Stand mod menu Stand not working stand mod I need help help mod menu mod menu help",
     answer: "Some common issues may be related to antivirus or firewall software. Try disabling them and see if that helps. If not, try running your mod as an administrator. If those options do not work,please contact us at"
   },
   // Add more FAQs here
